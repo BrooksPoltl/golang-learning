ff912dfabd21bd7726f564e1cc1330f71b4c12f5
BrooksPoltl <bpoltl1@gmail.com>
2020-02-15 14:52:49 -0600

    write to file and read

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
 
 func main() {
-	// cards := newDeck()
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-	greeting := "Hi there!"
-	fmt.Println([]byte(greeting))
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
