142d194efb36c2e25f5277b901db55dd32f66a90
BrooksPoltl <bpoltl1@gmail.com>
2020-02-15 07:51:07 -0600

    bitslice

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,13 @@
 package main
 
-func main() {
-	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
+import "fmt"
 
-	hand.print()
-	remainingCards.print()
+func main() {
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+	greeting := "Hi there!"
+	fmt.Println([]byte(greeting))
 }
