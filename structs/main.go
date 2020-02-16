16eeb0505d7758d602180a3d52d83cb835f95602
BrooksPoltl <bpoltl1@gmail.com>
2020-02-16 09:53:46 -0600

    declaring struct

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,13 @@
 package main
 
+import "fmt" 
+
 type person struct {
 	firstName string
 	lastName string
 }
 
 func main() {
-	
+	brooks := person{firstName: "Brooks", lastName: "Poltl"}
+	fmt.Println(brooks)
 }
