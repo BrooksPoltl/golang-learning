0c98c9582fddeddf156dcfb330a4d02214d59d68
BrooksPoltl <bpoltl1@gmail.com>
2020-02-19 18:26:51 -0600

    pritMap

--- map/main.go
+++ map/main.go
@@ -3,16 +3,16 @@ package main
 import "fmt" 
 
 func main() {
-	colors := make(map[string]string)
-	
-	// var colors map[string]string
+	colors := map[string]string {
+		"red": "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+	printMap(colors);
+}
 
-	// colors := map[string]string {
-	// 	"red": "#ff0000",
-	// 	"green": "#4bf745",
-	// }
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
