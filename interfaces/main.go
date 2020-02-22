60744a4e0ec072d54b3f7777e045d7f48e196f1f
BrooksPoltl <bpoltl1@gmail.com>
2020-02-22 10:50:46 -0600

    interface example

---
+++ interfaces/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+type bot interface {
+	getGreeting() string
+}
+type englishBot struct {}
+type spanishBot struct {}
+
+func main(){
+	eb:= englishBot{}
+	sb:= spanishBot{}
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (eb englishBot) getGreeting() string {
+	// VERY custom logic for generating english greeting
+	return "Hi There!"
+}
+func (sb spanishBot) getGreeting() string {
+	// VERY custom logic for generating english greeting
+	return "Hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
